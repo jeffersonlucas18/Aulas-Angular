2f46aee24a53c3a9c63e6f51ebd3edf31c7c32e2
Jefferson Lucas <jefferson@dimensiva.online>
2019-05-09 10:49:40 -0300

    criação do componente header e utilizando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'aula4', component:AppComponent},
+  {path:'aula20', component:HeaderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
